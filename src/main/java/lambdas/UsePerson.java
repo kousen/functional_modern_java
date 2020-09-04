c03ca45a65cbe645d86cfd71d085135c3931d871
Ken Kousen <ken.kousen@kousenit.com>
2020-09-03 21:15:54 -0400

    Add stream processing to UsePerson

--- src/main/java/lambdas/UsePerson.java
+++ src/main/java/lambdas/UsePerson.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,22 +8,29 @@ import java.util.stream.Collectors;
 public class UsePerson {
     @SuppressWarnings({"UnusedAssignment", "Convert2MethodRef"})
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("John", "Paul", "George", "Ringo");
+        List<String> names = List.of("John", "Paul", "George", "Ringo");
+
+        // Old-style Java 7- way
+        List<Person> beatles = new ArrayList<>();
+        for (String name : names) {
+            beatles.add(new Person(name));
+        }
+        System.out.println("Beatles: " + beatles);
 
         List<Person> people = names.stream()    // Stream<String>
-                .map(name -> new Person(name))  // Stream<Person>
-                .collect(Collectors.toList());  // Converts Stream<Person> to List<Person>
+                                   .map(name -> new Person(name))  // Stream<Person>
+                                   .collect(Collectors.toList());  // Converts Stream<Person> to List<Person>
+        System.out.println(people);
 
         people = names.stream()
-                .map(Person::new)
-                .collect(Collectors.toList());
-
+                      .peek(name -> System.out.println(Thread.currentThread().getName() + ": " + name))
+                      .map(Person::new)  // use the one-arg ctor in Person that takes a String arg
+                      .collect(Collectors.toList());
         System.out.println(people);
 
         Person[] peopleArray = names.stream()
-                .map(Person::new)
-                .toArray(Person[]::new);
-
+                                    .map(Person::new)
+                                    .toArray(Person[]::new);
         System.out.println(Arrays.toString(peopleArray));
     }
 }
