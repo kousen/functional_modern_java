05a09a059f83e563ca2b6e93076e47d34b341a72
Ken Kousen <ken.kousen@kousenit.com>
2020-09-03 20:55:36 -0400

    Show lazy error message using a Supplier

--- src/test/java/lambdas/FileFilterTest.java
+++ src/test/java/lambdas/FileFilterTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Map;
 
+import static java.util.Map.entry;
+import static java.util.Map.ofEntries;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SuppressWarnings("ConstantConditions")
@@ -46,11 +50,33 @@ public class FileFilterTest {
         assertEquals(16, dirs.length);
     }
 
+    private String getErrorMessage(int num) {
+        System.out.println("Creating the error message");
+        return "Should be " + num + " directories";
+    }
+
     @Test
     void listFiles_fileFilterMethodReference() {
         File root = new File("src/main/java");
         FileFilter filter = File::isDirectory;
         File[] dirs = root.listFiles(filter);
-        assertEquals(16, dirs.length);
+        assertEquals(16, dirs.length, () -> getErrorMessage(16));
+    }
+
+    @Test
+    void mapBiConsumerDemo() {
+        Map<String, Integer> map =
+                ofEntries(
+                        entry("a", 1),
+                        entry("b", 2),
+                        entry("c", 2));
+
+        map.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        assertAll(
+                () -> assertEquals(1, map.get("a")),
+                () -> assertEquals(2, map.get("b")),
+                () -> assertEquals(2, map.get("c"))
+        );
     }
 }
